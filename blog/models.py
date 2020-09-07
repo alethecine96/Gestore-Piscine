128d1d07558291e8fc85df0e47b8e94a83556457
alessandro <alethecine96@gmail.com>
2020-09-07 15:29:00 +0200

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.TextField()
+    content = models.TextField()
+    date_post = models.DateTimeField(default=timezone.now)    #auto_now_add=True
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+        
+        
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})

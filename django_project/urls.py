6ba3a0d20b4de17ed3bf817a1c95f78b2df0ef32
Luca0079 <luca.leuter@gmail.com>
2020-09-19 17:16:32 +0200

    review login mode

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,12 +20,13 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', user_views.log, name='login'),
+    path('login_request/', user_views.login_request, name='login_request'),
+    path('login_succ/', user_views.login_succ, name='login_succ'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
 
 def register(request):
     if request.method == 'POST':
@@ -40,3 +43,21 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
+    
+def log(request):
+    return render(request, 'users/login.html')
+
+@csrf_exempt
+def login_request(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    print(username)
+    print(password)
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('login_succ')        
+    return redirect('login')
+    
+def login_succ(request):
+    return render(request, 'users/login_succ.html')

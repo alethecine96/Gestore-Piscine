bb859cb0a4ee235ef947955810d967a5636ed5a3
Luca0079 <luca.leuter@gmail.com>
2020-09-19 19:15:22 +0200

    review login mode

--- users/views.py
+++ users/views.py
@@ -50,15 +50,20 @@ def log(request):
         return HttpResponse("<h1>Sei già autenticato</h1>")
     return render(request, 'users/login.html')
 
+
 @csrf_exempt
 def login_request(request):
     username = request.POST.get('username', None)
     password = request.POST.get('password', None)
+    print(request.POST.get('username', None))
+    print(request.POST.get('password', None))
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponse("<h1>Successo Login</h1>")     
+        if user.is_authenticated:
+            return HttpResponse("<h1>Successo Login</h1>")     
     return HttpResponse("<h1>Fallito Login</h1>")
+    
     
 def login_succ(request):
     return render(request, 'users/login_succ.html')

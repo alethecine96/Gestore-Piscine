679923ffd68043251e7693298f4f17cbdfff8a87
Luca0079 <luca.leuter@gmail.com>
2020-09-22 10:09:44 +0200

    add alert for ph

--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,8 @@
 from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    PiscinaListView,
-    MiaPiscinaListView
-    )
+from .views import PiscinaListView, MiaPiscinaListView
 from . import views
 
 urlpatterns = [
-    path('post', PostListView.as_view(), name='blog-post'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('mia_piscina/grafici', views.line_chart, name='line-chart'),
     path('', PiscinaListView.as_view(), name='blog-home'),
     path('piscina_request/', views.piscina_request, name='piscina_request'),
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from django.views.generic import (
-    ListView, 
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-    )
+from django.views.generic import ListView
 from .models import Post, Value
 from .forms import PiscinaForm
 from django.http import HttpResponse
@@ -86,52 +80,3 @@ def line_chart(request):
     else:
         messages.warning(request, 'Non hai effettuato l\'accesso')
         return redirect('login')
-
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-date_post']
-    
-    
-class PostDetailView(DetailView):
-    model = Post
-    
-    
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-        
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-    
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-    
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-    
-def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})

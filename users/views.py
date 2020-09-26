09d6ba0459520b9193afd56bcd3ab0a4f0e0b03b
Luca0079 <luca.leuter@gmail.com>
2020-09-26 17:35:56 +0200

    change messages

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, f'Il tuo account è stato creato con successo!')
             piscina = Piscina()
             usr = User.objects.last()
             piscina.user = usr
@@ -33,7 +33,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, f'Il tuo account è stato aggiornato correttamente!')
             return redirect('profile')
 
     else:
